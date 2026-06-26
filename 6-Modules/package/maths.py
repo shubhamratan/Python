79b459e1d7fbfb463fb14c168e219f1f30ffa6e5
Shubham Ratan <shubhamratan07@gmail.com>
2026-06-26 16:16:05 +0530

    Add AI, Flask, OpenAI projects and update Python repository

---
+++ 6-Modules/package/maths.py
@@ -0,0 +1,14 @@
+def addition(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiply(a,b,c):
+    return a*b*c
+
+def is_even(n):
+    if n%2==0:
+        return True
+    else:
+        return False
---
+++ 6-Modules/test.py
@@ -0,0 +1,8 @@
+from package.maths import*
+from package.subpackage.mult import multiplication
+
+print(addition(2,3))
+print(subtraction(7,2))
+print(multiply(2,3,4))
+print(multiplication(2,3))
+print(is_even(24))
